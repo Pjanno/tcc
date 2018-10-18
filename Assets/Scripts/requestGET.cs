f4752a078613e3840a1c72ea90c67b20a4eb12f6
Felip0e <frcchaves@hotmail.com>
2018-10-18 00:00:43 -0300

    Commit já com os arquivos do Kenzinho Por favor não trabalhe nessa branch, faça uma outra, cagar com isso é jogar tempo fora.

---
+++ Assets/Scripts/requestGET.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class requestGET : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    private string mensagem;
+
+	public IEnumerator GetText()
+    {
+        UnityWebRequest wr = UnityWebRequest.Get("http://localhost:8000/api/item");
+        yield return wr.SendWebRequest();
+
+        if(wr.isNetworkError || wr.isHttpError)
+        {
+            Debug.Log(wr.error);
+        }
+        else
+        {
+            // Exibe o JSON no log
+            Debug.Log(wr.downloadHandler.text);
+            mensagem = wr.downloadHandler.text;
+        }
+
+    }
+
+    public void EscreveSaporra()
+    {
+
+        print(mensagem);
+    }
+}
