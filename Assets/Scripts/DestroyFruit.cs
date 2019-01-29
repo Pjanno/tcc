2a65e720906d05e67536756510375da812d9de81
Gabriel Laranjeira <laranjeiragabriel@hotmail.com>
2019-01-29 19:47:49 -0200

    Alteração

---
+++ Assets/Scripts/DestroyFruit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFruit : MonoBehaviour {
+
+private GameObject[] Fruits;
+	
+void Update () { 
+	Destroy(gameObject, 6f); 
+	//Destroy Gameobject, after 2 Seconds. } 
+}
+
+private void OnCollisionEnter2D(Collision2D collision) { 
+	if(collision.gameObject.tag == "Ground") { 
+		//Destroy(gameObject); 
+		//Just destroy Gameobject, without delay. 
+		Destroy(gameObject, 6f); // Destroy GameObject after 5 Seconds. 
+	} 
+	if (collision.gameObject.tag == "Player") {
+		Destroy(gameObject);
+		Score.scoreValue += 1;
+	} 
+}  
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+public float maxSpeed;
+public float jumpForce;
+
+private bool grounded;
+private bool jumping;
+
+private Rigidbody2D rb;
+private Animator anim;
+private SpriteRenderer sprite;
+
+public Transform groundCheck;
+
+void Start ()
+{
+	rb = GetComponent<Rigidbody2D> ();
+	sprite = GetComponent<SpriteRenderer> ();
+	anim = GetComponent<Animator> ();
+}
+
+void Update ()
+{
+	grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+	
+	if (Input.GetButtonDown("Jump") && grounded)
+	{
+		jumping = true;
+	}
+}
+
+void FixedUpdate ()
+{
+	float move = Input.GetAxis("Horizontal");
+	rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
+
+	if ((move > 0f && sprite.flipX) || (move < 0f && !sprite.flipX))
+	{
+		Flip();
+	}
+
+	if (jumping) 
+	{
+		rb.AddForce(new Vector2(0f, jumpForce));
+		jumping = false;
+	}
+}
+
+void Flip ()
+{
+	sprite.flipX = !sprite.flipX;
+}
+
+}
