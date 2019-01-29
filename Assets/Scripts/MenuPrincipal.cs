34f6ff79433b2f16d4541eb3d96e13222a278834
Felip0e <frcchaves@hotmail.com>
2019-01-29 00:24:25 -0200

    Arquivos menu

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour {
+
+    public GameObject boxSair, menuPrincipal;
+
+    public void Start()
+    {
+        //Aqui provavelmente só é ativado quando roda a primeira vez esse script na scene
+        //então vamos inicializar o que é e o que não é pra ser visto
+        boxSair.SetActive(false);
+    }
+    
+    public void Jogar()
+    {
+        SceneManager.LoadScene("Level01", LoadSceneMode.Single);
+    }
+    public static void Config()
+    {
+        
+    }
+    public void Sair()
+    {
+        //Não sai efetivamente, pergunta primeiro com a outra box.
+        menuPrincipal.SetActive(false);
+        boxSair.SetActive(true);
+    }
+    public void SairEfetivo()
+    {
+        Application.Quit();
+    }
+    public void Voltar()
+    {
+        menuPrincipal.SetActive(true);
+        boxSair.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/generalSFX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generalSFX : MonoBehaviour {
+
+    public AudioSource sfx;
+    public AudioClip[] clp = new AudioClip[5]; //Setei como 5 audios somente
+    
+    
+    public void Start()
+    {
+        sfx.Stop(); //Pra não ter risco de tocar nada aleatório no começo
+    }
+
+    //Toca o audio do vetor com o parametro passado
+    public void StopAndPlaySfx(int i) 
+    {
+        sfx.Stop();
+        sfx.PlayOneShot(clp[i]);
+    }
+
+    //Toca o audio do vetor sem pausar o anterior
+    public void PlaySfx(int i)
+    {
+        sfx.Stop();
+        sfx.PlayOneShot(clp[i]);
+    }
+}
