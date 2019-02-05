c7d86100db0ab6413eaffc5253f8842e81721e16
Felip0e <frcchaves@hotmail.com>
2019-02-05 20:14:14 -0200

    Commit lendário de scenes novas e menu modificado

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -5,18 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour {
 
-    public GameObject boxSair, menuPrincipal;
+    public GameObject PainelSair, menuPrincipal;
+    
+
+    public void Awake()
+    {
+        Time.timeScale = 1;
+    }
 
     public void Start()
     {
         //Aqui provavelmente só é ativado quando roda a primeira vez esse script na scene
         //então vamos inicializar o que é e o que não é pra ser visto
-        boxSair.SetActive(false);
+        PainelSair.SetActive(false);
     }
     
     public void Jogar()
     {
-        SceneManager.LoadScene("Level01", LoadSceneMode.Single);
+        StartCoroutine(CarregarScene(1.5f, "Level01"));
     }
     public static void Config()
     {
@@ -26,15 +32,58 @@ public class MenuPrincipal : MonoBehaviour {
     {
         //Não sai efetivamente, pergunta primeiro com a outra box.
         menuPrincipal.SetActive(false);
-        boxSair.SetActive(true);
+        PainelSair.SetActive(true);
     }
     public void SairEfetivo()
     {
-        Application.Quit();
+        StartCoroutine(SairAplicacao(1.5f));
     }
     public void Voltar()
     {
         menuPrincipal.SetActive(true);
-        boxSair.SetActive(false);
+        PainelSair.SetActive(false);
+    }
+
+    // PARTE SONORA DO MENU SFX ========================================================
+    public AudioSource AudioSourceMenu;
+    public AudioClip[] sfx = new AudioClip[4]; // 0 = Click
+                                               // 1 = Hover
+                                               // 2 = Back
+                                               // 3 = Exit
+    public void Click()
+    {
+        AudioSourceMenu.Pause();
+        AudioSourceMenu.PlayOneShot(sfx[0]);
+    }
+    public void Hover()
+    {
+        AudioSourceMenu.Pause();
+        AudioSourceMenu.PlayOneShot(sfx[1]);
+    }
+    public void Back()
+    {
+        AudioSourceMenu.Pause();
+        AudioSourceMenu.PlayOneShot(sfx[2]);
+    }
+    public void Exit()
+    {
+        AudioSourceMenu.Pause();
+        AudioSourceMenu.PlayOneShot(sfx[3]);
+    }
+    
+    // COROUTINES ÚTEIS PARA AS ANIMAÇÕES AND SHIT LIKE THAT ==========================
+    IEnumerator CarregarScene(float tempo, string nomeScene)
+    {
+        Debug.Log("Estou aguardando " + tempo + " segundos para pular de linha");
+        yield return new WaitForSeconds(tempo);
+        Debug.Log("Pronto!");
+        SceneManager.LoadScene(nomeScene);
+    }
+
+    IEnumerator SairAplicacao (float segundos)
+    {
+        Debug.Log("Recebi o tempo de " + segundos + " segundos para fechar");
+        yield return new WaitForSeconds(segundos);
+        Application.Quit();
     }
 }
